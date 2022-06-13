3df49ab6612f70061280ed73175c266ca1d07a4f
irohasu <your.name@example.com>
2022-06-13 12:48:30 +0000

    Test is gomi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  
   def new
     @user = User.new
   end
@@ -16,11 +18,35 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+    
+    def edit 
+      @user = User.find(params[:id])
+    end
+    
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:success] = "Profile 更新しました！"
+        redirect_to @user
+      else
+        render 'edit'
+      end
+    end
   end
   
     private
   
       def user_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+      
+      #beforeアクション
+      
+      #ログイン済みユーザーかどうか確認
+      def logged_in_user
+        unless logged_in?
+          flash[:danger] = "Please log in."
+          redirect_to login_url
+        end
       end
 end
